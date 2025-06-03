14d8c17cab0535907927a215e468c939c7977808
lincheewei <cheewei1998@gmail.com>
2025-06-04 00:27:48 +0800

    added allowed host

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // Listen on all interfaces (0.0.0.0)
+    port: 5173,
+    allowedHosts: [
+      'localhost',
+      'ec2-43-216-11-51.ap-southeast-5.compute.amazonaws.com'
+    ],
     proxy: {
       '/api': 'http://localhost:9090'
     }
