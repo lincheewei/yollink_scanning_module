88ae134efa35ba53887d3fc4341c1c42cdb9f5f5
lincheewei <cheewei1998@gmail.com>
2025-07-15 18:15:38 +0800

    bat

--- start-yollink.bat
+++ start-yollink.bat
@@ -1,36 +1,36 @@
 @echo off
+setlocal
+
 set LOG=%TEMP%\yollink-startup.log
 echo [%date% %time%] Script started > "%LOG%"
 
-:: Navigate to script folder
+:: Go to this script's directory
 cd /d "%~dp0" || (
-    echo [%date% %time%] Failed to cd >> "%LOG%"
-    pause
+    echo [%date% %time%] Failed to cd to script folder >> "%LOG%"
     exit /b
 )
 
-:: Verify package.json
+:: Check if package.json exists
 if not exist package.json (
     echo [%date% %time%] ERROR: package.json not found >> "%LOG%"
-    pause
     exit /b
 )
 
-:: Start npm run dev using full path to npm
+:: Start npm run dev (log output separately)
 echo [%date% %time%] Starting npm dev >> "%LOG%"
-start /min cmd /k "cd /d \"%~dp0\" && \"C:\Program Files\nodejs\npm.cmd\" run dev >> %TEMP%\yollink-npm.log 2>&1"
+start "" /min cmd /k "cd /d \"%~dp0\" && \"C:\Program Files\nodejs\npm.cmd\" run dev >> \"%TEMP%\yollink-npm.log\" 2>&1"
 
-:: Wait 8 seconds before launching Chrome
+:: Wait before opening browser
 timeout /t 8 >nul
 
 :: Launch Chrome in kiosk mode
-set CHROME_PATH="C:\Program Files\Google\Chrome\Application\chrome.exe"
-if exist %CHROME_PATH% (
+set "CHROME_PATH=C:\Program Files\Google\Chrome\Application\chrome.exe"
+if exist "%CHROME_PATH%" (
     echo [%date% %time%] Launching Chrome >> "%LOG%"
-    start "" %CHROME_PATH% --kiosk "http://localhost:5173"
+    start "" "%CHROME_PATH%" --kiosk "http://localhost:5173"
 ) else (
     echo [%date% %time%] Chrome not found at %CHROME_PATH% >> "%LOG%"
 )
 
-:: End
 echo [%date% %time%] Script ended >> "%LOG%"
+endlocal
